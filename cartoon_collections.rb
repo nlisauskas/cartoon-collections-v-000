a6bf7b55bf5c92ec588d23c8de91666241f7c0eb
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-10 12:20:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(calls)
     false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
